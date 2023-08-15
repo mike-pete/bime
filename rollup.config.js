dbe6c608ab150338f7c512d2f808403e3f7ba281
Mike <mike@lemonshell.com>
2023-08-15 13:39:12 -0600

    add minification

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,18 @@
-import typescript from '@rollup/plugin-typescript';
+import typescript from '@rollup/plugin-typescript'
+import terser from '@rollup/plugin-terser';
 
 export default {
-  input: 'src/bime.ts',
-  output: {
-    dir: 'dist',
-    format: 'es'
-  },
-  plugins: [typescript()]
-};
+	input: 'src/bime.ts',
+	output: [
+		{
+			file: './dist/bime.js',
+			format: 'es',
+		},
+		{
+			format: 'es',
+			file: './dist/bime.min.js',
+			plugins: [terser()],
+		},
+	],
+	plugins: [typescript()],
+}
