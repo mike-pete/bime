04a3218467f29b2b04c2a51a21f0635394657b1f
Mike Peterson <mike@lemonshell.com>
2024-01-25 17:30:14 -0700

    add rollup

---
+++ rollup.config.js
@@ -0,0 +1,12 @@
+import typescript from '@rollup/plugin-typescript'
+
+export default {
+	input: 'src/bime.ts',
+	output: [
+		{
+			file: './dist/bime.js',
+			format: 'es',
+		},
+	],
+	plugins: [typescript()],
+}
