90e28d8f1eabf0fd2c96e74b3e9b8fbf0eea4c06
Mike Peterson <mike@lemonshell.com>
2024-01-25 17:37:31 -0700

    remove dist

--- dist/bime.d.ts
+++
@@ -1,6 +0,0 @@
-type Model = Record<string, (...args: any[]) => any>;
-type MessageResponse<T> = {
-    [K in keyof T]: T[K] extends (...args: infer A) => infer R ? (...args: A) => Promise<R> : never;
-};
-declare const bime: <T extends Model>(target: Window) => MessageResponse<T>;
-export default bime;
