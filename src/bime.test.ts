35c36b62e9e272dd0b075334e8cae1a208b8d4eb
Mike Peterson <mike@lemonshell.com>
2025-04-23 15:49:11 -0700

    add tests for cleanup

--- src/bime.test.ts
+++ src/bime.test.ts
@@ -1,15 +1,17 @@
-import { expect, test } from "bun:test"
+import { expect, jest, test } from "bun:test"
 import { EventEmitter } from "node:events"
 import bime from "./bime"
 
 function createEvent() {
   const id = crypto.randomUUID()
+  const cleanupMock = jest.fn()
   const event = new EventEmitter()
   const listener = (handler: (message: string) => void) => {
     event.on(id, handler)
 
     return () => {
       event.off(id, handler)
+      cleanupMock()
     }
   }
 
@@ -20,13 +22,17 @@ function createEvent() {
   return {
     listener,
     sender,
+    cleanupMock,
   }
 }
 test("example test", async () => {
-  const { listener, sender } = createEvent()
+  const { listener, sender, cleanupMock } = createEvent()
 
   const model = {
     sayHello: (name: string) => `Hello ${name}`,
+    print: (message: string) => {
+      console.log(message)
+    },
   }
 
   const listen = bime.listen({ model, listener, sender })
@@ -36,10 +42,20 @@ test("example test", async () => {
 
   // @ts-expect-error: This is necessary because the method 'notDefined' does not exist in the model.
   expect(() => invoke.notDefined()).toThrow()
+  expect(async () => {
+    await invoke.print("it works!")
+  }).not.toThrow()
 
   expect(listen.cleanup).toBeDefined()
   expect(invoke.cleanup).toBeDefined()
 
   listen.cleanup()
+  expect(cleanupMock).toHaveBeenCalled()
   invoke.cleanup()
+  expect(cleanupMock).toHaveBeenCalled()
+
+  const promise = (async () => {
+    void invoke.sayHello("Mike")
+  })()
+  expect(promise).rejects.toThrow()
 })
